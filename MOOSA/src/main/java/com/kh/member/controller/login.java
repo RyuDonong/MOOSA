2567beec1948731a6ed982f0c7f5c88023fb1bc0
khacademy <khacademy@user1>
2024-05-02 16:55:58 +0900

    로그인 완성 야호 ~

--- MOOSA/src/main/java/com/kh/member/controller/login.java
+++ MOOSA/src/main/java/com/kh/member/controller/login.java
@@ -6,22 +6,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.kh.member.model.service.MemberService;
-import com.kh.member.model.vo.Member;
 
 /**
- * Servlet implementation class LoginContoller
+ * Servlet implementation class login
  */
-@WebServlet("/login.me")
-public class LoginContoller extends HttpServlet {
+@WebServlet("/loginPage.go")
+public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginContoller() {
+    public login() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,30 +26,15 @@ public class LoginContoller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("views/common/loginPage.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-request.setCharacterEncoding("UTF-8");
-		
-		String userId = request.getParameter("userId");
-		String userPwd = request.getParameter("userPwd");
-		
-		Member loginUser = new MemberService().loginMember(userId,userPwd);
-		
-		HttpSession session = request.getSession();
-		
-		if(loginUser==null) {
-			System.out.println("로그인 실패");
-		}else {
-			session.setAttribute("loginUSer", loginUser);
-			System.out.println("로그인 성공");
-			
-			response.sendRedirect(request.getContextPath());
-		}
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
-	
 
 }
